f831eca228a429ed06834414326506b8d3f9bb8a
Vinod Kumar <125216677+vinod2400@users.noreply.github.com>
2025-07-20 17:49:52 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export function generateUniqueId(): string {
+  return Math.random().toString(36).substr(2, 9);
+}
