0987d3cc03ab461cb9ae0d0b0a393fd9d1966657
Vinod Kumar <125216677+vinod2400@users.noreply.github.com>
2025-07-20 17:49:05 +0530

    Update FieldEditor.tsx

--- src/components/FieldEditor.tsx
+++ src/components/FieldEditor.tsx
@@ -4,7 +4,6 @@ import { Input } from "./ui/input";
 import {
   Collapsible,
   CollapsibleContent,
-  CollapsibleTrigger,
 } from "./ui/collapsible";
 import { Label } from "./ui/label";
 import { useFieldArray, useFormContext } from "react-hook-form";
